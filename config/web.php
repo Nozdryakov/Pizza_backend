b2f36bbffbbe892d6d7ddfa8c9b14a1cf53154f3
Nozdryakov <nozdryakovtema0603@gmail.com>
2024-04-30 03:50:08 +0300

    fix cors

--- config/web.php
+++ config/web.php
@@ -25,6 +25,7 @@ $config = [
             'on beforeSend' => function ($event) {
                 $response = $event->sender;
                 $response->headers->add('Access-Control-Allow-Origin', 'http://localhost:5173');
+                $response->headers->add('Access-Control-Allow-Origin', 'http://localhost:5173');
                 $response->headers->add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
                 $response->headers->add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             },
